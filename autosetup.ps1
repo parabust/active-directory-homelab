948fa53a3cc36055d302d75ec89b235d984c1129
parabust <oukeddie@gmail.com>
2025-07-08 16:53:35 -0400

    Fixes and new checks to autosetup

--- autosetup.ps1
+++ autosetup.ps1
@@ -6,7 +6,7 @@ Set-Item wsman:\localhost\Client\TrustedHosts -value $domainControllerIP -Force
 $session = New-PSSession -ComputerName $domainControllerIP -Credential (Get-Credential)
 
 $sessionAdminStatus = [bool] (Invoke-Command -Session $session -ScriptBlock {
-    net localgroup administrators -contains (whoami).split("\")[-1]
+    (net localgroup administrators) -contains (whoami).split("\")[-1]
 })
 if ($sessionAdminStatus -eq $false) {
     Write-Host "This account on the domain controller doesn't have admin privileges"
@@ -14,15 +14,36 @@ if ($sessionAdminStatus -eq $false) {
 }
 
 Invoke-Command -Session $session -ScriptBlock {
-    if ((Get-WindowsFeature -Name AD-Domain-Services).installState -eq "Available") {
+    if ((Get-WindowsFeature -Name AD-Domain-Services).InstallState -eq "Available") {
         Install-WindowsFeature AD-Domain-Services -IncludeManagementTools -Force
         Import-Module ADDSDeployment
         Install-ADDSForest
     }
 }
 
+$testConnectionCounter = 1
+$continueTestConnection = $true
+while ($continueTestConnection -eq $true) {
+    while ($testConnectionCounter -le 10) {
+        $testConnection = Test-NetConnection -ComputerName $domainControllerIP
+        if ($testConnection.PingSucceeded -eq $true) {
+            $continueTestConnection = $false
+            break
+        }
+        Start-Sleep -Seconds 15
+        Write-Host "Attempting reconnect to domain controller.. ($($testConnectionCounter))"
+        $testConnectionCounter++
+    }
+    Write-Host "Couldn't reconnect to domain controller, maybe slow restart or DNS issue?"
+    $continueTestConnectionChoice = Read-Host "Retry connection to domain controller? ('y' to retry)"
+    if ($continueTestConnectionChoice.toLower() -ne "y") {
+        exit
+    }
+    $testConnectionCounter = 1   
+}
+
 $domain = [string] (Invoke-Command -Session $session -ScriptBlock {
     (Get-ADDomain).DNSRoot
 })
-Add-Computer -DomainName $domain -Credential ($domain.split(".")[0] + "\\") -Force -Restart
+Add-Computer -DomainName $domain -Credential ($domain.split(".")[0] + "\") -Force -Restart
 
